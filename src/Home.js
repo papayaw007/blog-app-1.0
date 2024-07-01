f90b36a9a2bccf60367def28e922c921a110db18
papayaw007 <98191059+papayaw007@users.noreply.github.com>
2024-07-01 17:02:42 +0000

    Update Home.js
    changed the link to fetch data

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const { data:blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
+    const { data:blogs, isPending, error} = useFetch("http://https://blog-app-1-0-lilac.vercel.app/blogs");
 
     return ( 
         <div className="home">
